8faf137c9e564b117dc81fcdbec2f030fe7b2b53
gabreurt <gabriel.torres@sou.inteli.edu.br>
2022-11-28 10:32:43 -0300

    Aperfeiçoamento do front-end e GET de histórico

--- src/frontend/components/Sala/Sala.jsx
+++ src/frontend/components/Sala/Sala.jsx
@@ -7,7 +7,7 @@ function Sala({ numeroSala }) {
             <div className="flex flex-col justify-center lg:items-center">
                 {mock.map((element) => (
                     <Link href={"/equipamentos"} key={element.numero}>
-                        <div  className="flex justify-center items-center border-solid border-2 border-black rounded-lg h-11 m-2   lg:w-96 cursor-pointer hover:p-6 hover:shadow-cyan-700 hover:shadow-lg">
+                        <div  className="flex justify-center items-center border-solid border-2 border-black rounded-lg h-11 m-2   lg:w-96 cursor-pointer hover:p-6 transition-all duration-700 ease-in delay-10 hover:shadow-cyan-700 hover:shadow-lg">
                             <h1>Sala {element.numero}</h1>
                         </div>
                     </Link>
--- src/frontend/pages/equipamentos.jsx
+++ src/frontend/pages/equipamentos.jsx
@@ -1,21 +1,26 @@
 import Link from "next/link";
 import ContainerEquipamentos from "../components/ContainerEquipamentos/ContainerEquipamentos";
-
+import {faHouse, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from '../styles/equipamentos.module.css'
 
 
 function equipamentos() {
     return (
         <div className="text-center mt-5">
-            <button className="bg-transparent border-2 border-cyan-600 rounded-md hover:bg-cyan-400 hover:border-transparent">
-                <Link href="/salas">Voltar</Link>
-            </button>
-
             <h1 className="text-lg font-bold">Sala x - Equipamentos</h1>
-            <input type="text" className="border-2 border-black mb-5 rounded-xl " placeholder="ID equipamento" />
-
+            <div className="justify-center flex">
+                    <div className="rounded-lg w-80">
+                        <FontAwesomeIcon className="pr-5" icon={faMagnifyingGlass}></FontAwesomeIcon>
+                        <input className="w-70 border-hidden justify-center" placeholder="Digite o ID que deseja!" ></input>
+                    </div>
+                </div>
             <div>
                 <ContainerEquipamentos></ContainerEquipamentos>
             </div>
+        <button id={styles.margin} className="bg-transparent border-2 rounded-full border-cyan-600 w-10 hover:bg-cyan-400 hover:border-transparent">
+            <Link href="/salas"><FontAwesomeIcon icon={faHouse}></FontAwesomeIcon></Link>
+        </button>
         </div>
     )
 }
