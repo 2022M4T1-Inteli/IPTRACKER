f7f83c2e73f38bb991501ef11a3fa4b73d2ee13a
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2022-11-30 15:24:58 -0300

    Delete comentários and search bar
    Exclusão de comentários desnecessários e ajuste na barra de pesquisas da página de baterias.

--- src/frontend/pages/bateria.jsx
+++ src/frontend/pages/bateria.jsx
@@ -10,41 +10,12 @@ function Bateria() {
     return (
         <div className="text-center"> {/*abertura da div principal que está centralizando o conteúdo*/}
 
-
-            <div className="container"> {/*div com título, ícone e input*/}
-                {/* <h1 className="Montserrat font-bold text-2xl text-black lg:pl-28 bg-sky-500 hover:bg-cyan-600">STATUS DA BATERIA</h1> */}
+            <div className="relative">
                 <h1 className="Montserrat font-bold text-2xl text-black lg:pl-28">Status da Bateria</h1>
-                <span class="icon" ><FontAwesomeIcon icon={faMagnifyingGlass} size="lg" position="absolute" /></span>
-                <input type="text" className={styles.input} placeholder="Insira o código do equipamento" />
+                <FontAwesomeIcon className="absolute lg:pl-32 pt-7 h-7 sm:pl-5" icon={faMagnifyingGlass}></FontAwesomeIcon>
+                <input type="text" className={styles.input} placeholder="Digite o número do prédio" />
             </div>
 
-            {/* <div className="flex flex-row flex justify-center">
-                <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
-                <p className="Montserrat font-bold blue 600">O objetivo era que a lupa estivesse no input, mas não está indo. O FontAwesome está aqui, no entanto.</p>
-            </div> */}
-
-
-
-            {/* flex justify-center items-center border-solid border-2 border-black rounded-lg h-11 m-2 lg:w-96 cursor-pointer hover:p-6 hover:shadow-cyan-700 hover:shadow-lg */}
-            <div className="flex flex-row flex justify-center  grid grid-cols-1 gap-3 md:grid-cols-6">
-                {/* <div>
-                    <div className="border-dotted border-2 border-indigo-600"><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div className="border-dotted border-2 border-indigo-600"><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div className="border-dotted border-2 border-indigo-600"><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div className="border-dotted border-2 border-indigo-600"><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div className="border-dotted border-2 border-indigo-600"><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <div><FontAwesomeIcon icon={faBatteryQuarter} size="lg"/></div>
-                    <FontAwesomeIcon icon={faBatteryQuarter} size="lg"/>
-                    <FontAwesomeIcon icon={faBatteryQuarter} size="lg"/>
-                    <FontAwesomeIcon icon={faBatteryQuarter} size="lg"/>
-                </div> */}
-
-
-            </div>
 
             <div className="flex justify-center">
                 <div className="w-3/4">
